047a5bed7ef0fecdafd075a2e915bc2b24f3abfe
Andrii Lishchyna <andrej044@i.ua>
2020-09-14 20:15:26 +0200

    function getter for X and Y possition

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //  PseudoCode
+
+// pobieranie listy
+
+const listItems = document.querySelectorAll('.paperList > li');
+
+let mouseXpos = 0;
+let mouseYpos = 0;
+
+// getter for X and Y mouse coordinates
+const getMousePos = () => {
+    document.body.addEventListener('mousemove', (e) => {
+        mouseXpos = e.clientX;
+        mouseYpos = e.clientY;
+        console.log(mouseXpos, mouseYpos);
+    })
+}
+getMousePos();
