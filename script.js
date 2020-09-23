796c2e83ed8e8f1bc8fcd3a71777dda418a01990
Andrii Lishchyna <andrej044@i.ua>
2020-09-23 18:56:00 +0200

    add new functions: chooseElem, changeInput

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const data = {
     left: 10,
     countItem: 10,
     mainBoard: document.querySelector(".mainBoard"),
+    cards: document.querySelector(".cards"),
+    inputValue: document.getElementById('inputValue'),
+    submit: document.getElementById('submit'),
+    index: 9,
+
 }
 
 
@@ -14,15 +19,22 @@ class Item {
         for (let i = 0; i < data.countItem; i++) {
             this.elem = document.createElement('div');
             this.elem.classList.add('block');
-            data.left = data.left + 100;
+            data.left = data.left + 2;
+            data.top = data.top + 2;
             this.elem.style.left = `${data.left}px`;
-            data.mainBoard.appendChild(this.elem);
+            this.elem.style.top = `${data.top}px`;
+            data.cards.appendChild(this.elem);
             this.elem.textContent = i;
+
         }
     }
-
+    //realize move method for a div block
     moveElem() {
         const divAll = document.querySelectorAll('.block');
+        // document.body.addEventListener('mousemove', (e) => {
+        //     console.log(e.clientX, e.offsetY);
+        // })
+
         divAll.forEach(divOne => {
             let posX;
             let posY;
@@ -35,16 +47,18 @@ class Item {
                 divOne.style.backgroundColor = "gray";
                 divOne.style.zIndex = ++zIndex;
                 flag = true;
-                console.log("x:" + posX, "y:" + posY)
+                // console.log("x:" + posX, "y:" + posY)
 
             })
             divOne.addEventListener('mousemove', (e) => {
                 if (flag) {
-                    divOne.style.left = `${e.clientX-posX}px`;
+                    divOne.style.left = `${e.clientX-posX - data.mainBoard.offsetLeft}px`;
                     divOne.style.top = `${e.clientY-posY}px`
-
-                    console.log("x:" + e.clientX, "y:" + e.clientY)
+                    // console.log("x:" + e.clientX, "y:" + e.clientY)
                 }
+            })
+            divOne.addEventListener('mouseout', () => {
+                flag = false;
             })
 
             divOne.addEventListener('mouseup', (e) => {
@@ -54,8 +68,53 @@ class Item {
         })
     }
 
+
+    // wybór elementa
+    chooseElem() {
+        const divAll = document.querySelectorAll('.block');
+
+
+        divAll.forEach((item, index) => {
+            item.addEventListener('click', function () {
+                data.index = index;
+            })
+        })
+
+    }
+
+    // work on the input form 
+    changeInput() {
+        const value = data.inputValue.value;
+        const blockAll = [...document.querySelectorAll('.block')];
+        for (let i = 0; i < blockAll.length; i++) {
+            let ul = document.createElement('ul');
+
+            blockAll[i].appendChild(ul);
+        }
+
+        data.inputValue.addEventListener('focusin', () => {
+            data.inputValue.value = "";
+            item.chooseElem();
+            console.log(data.index)
+        })
+
+        // data.inputValue.addEventListener('focusout', () => {
+        //     data.inputValue.value = value;
+        // })
+
+        data.submit.addEventListener('click', (e) => {
+            if (data.inputValue.value != "") {
+                let num = data.index + 1;
+                const li = document.createElement('li');
+                document.querySelector(`.block:nth-of-type(${num})>ul`).appendChild(li).textContent = data.inputValue.value;
+                data.inputValue.value = "";
+            }
+        })
+    }
+
 }
 
 
 const item = new Item();
+item.changeInput();
 item.moveElem();
